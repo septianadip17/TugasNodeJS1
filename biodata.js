076133bb4c3d2c1779b77efa4be68e8fc7d75713
septianadip17 <septianadip7998@gmail.com>
2023-05-09 23:40:10 +0700

    second commit

---
+++ biodata.js
@@ -0,0 +1,7 @@
+// Module untuk menampilkan biodata
+module.exports = {
+  nama: "Septian Adi Pratama",
+  tempatLahir: "Bogor",
+  tanggalLahir: "7 September 1998",
+  alamat: "Jl. Krukut Raya",
+};
---
+++ moduleBiodata.js
@@ -0,0 +1,6 @@
+const biodata = require("./biodata");
+
+console.log(`Nama: ${biodata.nama}`);
+console.log(`Tempat Lahir: ${biodata.tempatLahir}`);
+console.log(`Tanggal Lahir: ${biodata.tanggalLahir}`);
+console.log(`Alamat: ${biodata.alamat}`);
